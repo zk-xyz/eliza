625816eb7306454bb54bb98faa729598358f4518
meepzorp <115584429+meepzorp@users.noreply.github.com>
2025-01-10 02:52:51 -0800

    client vite config update for agent render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -30,10 +30,9 @@ export default defineConfig({
         host: true,
         proxy: {
             "/api": {
-                target: `http://127.0.0.1:${process.env.SERVER_PORT || 3000}`,
+                target: process.env.VITE_SERVER_URL || `http://127.0.0.1:${process.env.SERVER_PORT || 3000}`,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
         },
     },
-});
